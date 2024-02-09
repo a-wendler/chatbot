f78b564c69ba8deeb6f102966a32e28c0eb1d93f
André Wendler <42637005+a-wendler@users.noreply.github.com>
2024-02-09 07:58:54 +0000

    add pplx bot

---
+++ chatbot/bot-pplx.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import Perplexity
+# import openai
+from llama_index import SimpleDirectoryReader
+
+# openai.api_key = st.secrets.openai_key
+pplx_api_key = st.secrets.pplx_key
+llm = Perplexity(
+    api_key=pplx_api_key, model="pplx-70b-chat", temperature=0.4, system_prompt="Du bist ein Experte für die Leipziger Städtischen Bibliotheken. Du hilfst Nutzerinnen und Nutzern dabei, die Bibliothek zu benutzen. Du beantwortest Fragen zum Ausleihbetrieb, zu den Standorten und den verfügbaren Services. Deine Antworten sollen auf Fakten basieren. Halluziniere keine Informationen über die Bibliotheken, die nicht auf Fakten basieren. Wenn Du eine Information über die Bibliotheken nicht hast, sage den Nutzenden, dass Du Ihnen nicht weiterhelfen kannst. Antworte immer in der Sprache, in der die Frage gestellt wurde."
+)
+
+st.header("Der LSB-Service-Chat 💬 📚")
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Was möchten Sie über die Leipziger Städtischen Bibliotheken wissen?"}
+    ]
+
+@st.cache_resource(show_spinner=True)
+def load_data():
+    with st.spinner(text="Die LSB-Informationen werden indiziert. Das dauert nur ein paar Augenblicke."):
+        reader = SimpleDirectoryReader(input_dir="chatbot/data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm)
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+# chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+chat_engine = index.as_chat_engine(chat_mode="context", verbose=True)
+
+if prompt := st.chat_input("Ihre Frage"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Ich denke nach ..."):
+            response = chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
